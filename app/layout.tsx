db70b7cbfbc1386766c0a4dba6f10e167d7365ab
runadeesu <sekainoruna@protonmail.com>
2026-07-12 18:38:37 +0900

    Delete app directory

--- app/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ja">
-      <body style={{ margin: 0, background: '#0b0b0f', color: '#f5f5f5' }}>{children}</body>
-    </html>
-  );
-}
